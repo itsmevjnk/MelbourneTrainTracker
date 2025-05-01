7ecd304e1ddfa5bd5de5c79e4e0b209aec5c2746
Vinh Nguyen <ngtv0404@gmail.com>
2025-05-02 02:12:19 +1000

    make colours look better
    might be a bit dim

--- MTT_Firmware/include/colours.h
+++ MTT_Firmware/include/colours.h
@@ -2,16 +2,16 @@
 
 #pragma once
 
-// taken from the PTV Visual Identity Toolkit 2.0: https://archive.org/details/ptv-vit
+// inspired by the PTV Visual Identity Toolkit 2.0: https://archive.org/details/ptv-vit
 typedef enum {
-    kSandringham = 0xF178AF,
-    kCrossCity = 0x028430,
-    kDandenong = 0x279FD5,
-    kBurnley = 0x152C6B,
-    kNorthern = 0xFFBE00,
-    kClifton = 0xBE1014,
-    kFlemington = 0x95979A,
-    kSpecial = 0x95979A, // e.g. on the City Circle
-    kVLine = 0x7F0D82,
+    kSandringham = 0xA0050A, // oddly enough the Clifton Hill colour works well for Sandringham (with some tweaks to reduce brightness)
+    kCrossCity = 0x008000,
+    kDandenong = 0x181880,
+    kBurnley = 0x000080,
+    kNorthern = 0x804000,
+    kClifton = 0x800000,
+    kFlemington = 0x606060, 
+    kSpecial = 0x606060, // e.g. on the City Circle
+    kVLine = 0x400080,
     kOff = 0x000000, // LED off
 } colour_t;

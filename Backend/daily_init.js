6a0ee2af770ab50f01be6a11520de02f85211b45
Vinh Nguyen <ngtv0404@gmail.com>
2025-05-04 02:21:22 +1000

    add next station to daily timetable

--- Backend/daily_init.js
+++ Backend/daily_init.js
@@ -5,17 +5,22 @@ const { TableName, ColumnSet, insert, select } = pgp.helpers;
 
 const daily_init = () => {
     const query = `
-        INSERT INTO daily.timetable (trip_id, line, seq, station, arrival, departure)
+        INSERT INTO daily.timetable (trip_id, line, seq, station, arrival, departure, next_station)
         SELECT
             A.trip_id AS trip_id,
             (REGEXP_MATCHES(A.trip_id, '[A-Z]{3}'))[1] AS line,
             A.seq AS seq,
             B.station AS station,
             CURRENT_DATE + A.arrival AS arrival,
-            CURRENT_DATE + A.departure AS departure
+            CURRENT_DATE + A.departure AS departure,
+            B2.station AS next_station
         FROM gtfs.timetable A
         INNER JOIN gtfs.stops B
-        ON A.stop_id = B.id
+            ON A.stop_id = B.id
+        LEFT JOIN gtfs.timetable A2
+            ON A.trip_id = A2.trip_id AND A.seq + 1 = A2.seq
+        LEFT JOIN gtfs.stops B2
+            ON A2.stop_id = B2.id
         WHERE A.trip_id IN (
             SELECT id
             FROM (
@@ -36,7 +41,7 @@ const daily_init = () => {
                         AND start_date <= CURRENT_DATE
                         AND end_date >= CURRENT_DATE
                 )
-            )
+            ) filtered_trips
             WHERE line IN (
                 'ALM', 'BEG', 'CBE', 'CCL', 'CGB', 'FKN', 'GWY', 'HBE', 'LIL', 'MDD', 'PKM', 'RCE', 'SHM', 'STY', 'SUY', 'UFD', 'WER', 'WIL',
                 'ART', 'BAT', 'BDE', 'GEL', 'MBY', 'TRN', 'vPK', 'WBL'

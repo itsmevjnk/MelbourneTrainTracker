b2c5c4f3c1b10958fd8d1fd72b72d5b755aa901e
Vinh Nguyen <ngtv0404@gmail.com>
2025-05-03 16:18:33 +1000

    add GTFS timetable download

---
+++ Backend/download_gtfs.js
@@ -0,0 +1,325 @@
+const temp = require('temp');
+const fs = require('fs');
+const stream = require('stream');
+const progress = require('progress-stream');
+const yauzl = require('yauzl');
+const csv = require('csv-parse');
+
+temp.track(); // automatically remove file on completion
+
+const GTFS_DATASET_URL = process.env.GTFS_DATASET_URL || 'https://opendata.transport.vic.gov.au/dataset/3f4e292e-7f8a-4ffe-831f-1953be0fe448/resource/5a29bfc4-2396-4bf5-860c-473e5b292f3c/download/gtfs.zip';
+// const GTFS_DATASET_URL = 'http://192.168.42.69/gtfs.zip';
+
+const createProgressStream = (length) => {
+    const progressStream = progress({
+        length: length,
+        time: 1000
+    });
+    const lengthMB = length / 1048576;
+    const lengthStr = lengthMB.toFixed(1);
+    progressStream.on('progress', (progress) => {
+        const transferMB = progress.transferred / 1048576;
+        const speedMB = progress.speed / 1048576;
+        console.log(`${progress.percentage.toFixed(0).padStart(3)}% | ${transferMB.toFixed(1).padStart(lengthStr.length)}/${lengthStr} MiB transferred (${speedMB.toFixed(1)} MiB/s)`);
+    });
+
+    return progressStream;
+};
+
+const convertDate = (dateString) => { // convert YYYYMMDD date string to Date object
+    return new Date(dateString.slice(0,4), dateString.slice(4,6) - 1, dateString.slice(6));
+};
+
+const parseCalendar = (stream) => {
+    return new Promise((resolve, reject) => {
+        const parser = csv.parse({ delimiter: ',', columns: true, bom: true });
+        parser.on('error', (err) => {
+            reject(err);
+        });
+        const entries = {};
+        parser.on('readable', () => {
+            let record;
+            while ((record = parser.read()) !== null) {
+                entries[record.service_id] = {
+                    startDate: convertDate(record.start_date),
+                    endDate: convertDate(record.end_date),
+                    monday: record.monday == '1',
+                    tuesday: record.tuesday == '1',
+                    wednesday: record.wednesday == '1',
+                    thursday: record.thursday == '1',
+                    friday: record.friday == '1',
+                    saturday: record.saturday == '1',
+                    sunday: record.sunday == '1',
+                };
+            }
+        });
+        parser.on('end', () => {
+            resolve({ calendar: entries });
+        });
+        stream.pipe(parser);
+    });
+};
+
+const parseCalendarDates = (stream) => {
+    return new Promise((resolve, reject) => {
+        const parser = csv.parse({ delimiter: ',', columns: true, bom: true });
+        parser.on('error', (err) => {
+            reject(err);
+        });
+        const entries = {};
+        parser.on('readable', () => {
+            let record;
+            while ((record = parser.read()) !== null) {
+                entries[record.service_id] = convertDate(record.date);
+            }
+        });
+        parser.on('end', () => {
+            resolve({ calendarDates: entries });
+        });
+        stream.pipe(parser);
+    });
+};
+
+const resolveVLineStation = (name) => {
+    name = name.split('Railway')[0].trim(); // trim name to get station name
+    return {
+        'Melton': 'MEL',
+        'Cobblebank': 'TLN',
+        'Rockbank': 'RBK',
+        'Caroline Springs': 'CPS',
+        'Wyndham Vale': 'WVL',
+        'West Tarneit': 'DAV',
+        'Tarneit': 'TNT',
+        'Deer Park': 'DEK',
+        'Ardeer': 'ADR',
+        'Sunshine': 'SUN',
+        'Footscray': 'FSY',
+        'Southern Cross': 'SSS',
+        'Flinders Street': 'FSS',
+        'Richmond': 'RMD',
+        'Caulfield': 'CFD',
+        'Clayton': 'CLA',
+        'Dandenong': 'DNG',
+        'Berwick': 'BEW',
+        'Pakenham': 'PKM'
+    }[name];
+};
+
+const parseStops = (stream) => {
+    return new Promise((resolve, reject) => {
+        const parser = csv.parse({ delimiter: ',', columns: true, bom: true });
+        parser.on('error', (err) => {
+            reject(err);
+        });
+        const entries = {};
+        parser.on('readable', () => {
+            let record;
+            while ((record = parser.read()) !== null) {
+                const stopID = record.stop_id * 1;
+                if (isNaN(stopID)) continue; // non-numeric stop ID - skip (all stop IDs in stop_times.txt are numeric)
+                const stationCodeMatch = record.parent_station.match(/[A-Z]{3}/);
+                const stationCode = (stationCodeMatch === null) ? resolveVLineStation(record.stop_name) : stationCodeMatch[0];
+                if (stationCode === undefined) continue; // station code not in metropolitan V/Line stations - skip
+                if (!entries.hasOwnProperty(stationCode)) entries[stationCode] = []; // create array of stop IDs
+                entries[stationCode].push(stopID); // convert string to number for more compact storage
+            }
+        });
+        parser.on('end', () => {
+            resolve({ stops: entries });
+        });
+        stream.pipe(parser);
+    });
+};
+
+const convertTime = (timeString) => { // convert hh:mm:ss time string to time offset from 00:00:00
+    const ret = new Date(0);
+    ret.setUTCHours(timeString.slice(0,2));
+    ret.setUTCMinutes(timeString.slice(3,5));
+    ret.setUTCSeconds(timeString.slice(6,9));
+    return ret.getTime();
+};
+
+const parseStopTimes = (stream) => {
+    return new Promise((resolve, reject) => {
+        const parser = csv.parse({ delimiter: ',', columns: true, bom: true });
+        parser.on('error', (err) => {
+            reject(err);
+        });
+        const entries = {};
+        parser.on('readable', () => {
+            let record;
+            while ((record = parser.read()) !== null) {
+                if (!entries.hasOwnProperty(record.trip_id)) entries[record.trip_id] = {};
+                entries[record.trip_id][record.stop_sequence * 1] = {
+                    stopID: record.stop_id * 1,
+                    arrivalTime: convertTime(record.arrival_time),
+                    departureTime: convertTime(record.departure_time)
+                };
+            }
+        });
+        parser.on('end', () => {
+            resolve({ times: entries });
+        });
+        stream.pipe(parser);
+    });
+};
+
+const parseTrips = (stream) => {
+    return new Promise((resolve, reject) => {
+        const parser = csv.parse({ delimiter: ',', columns: true, bom: true });
+        parser.on('error', (err) => {
+            reject(err);
+        });
+        const entries = {};
+        parser.on('readable', () => {
+            let record;
+            while ((record = parser.read()) !== null) {
+                entries[record.trip_id] = record.service_id;
+            }
+        });
+        parser.on('end', () => {
+            resolve({ tripCalendar: entries });
+        });
+        stream.pipe(parser);
+    });
+};
+
+const extractFeed = (readStream) => {
+    const feed = temp.createWriteStream('gtfs_feed');
+    return new Promise((resolve, reject) => {
+        readStream.on('end', () => { // on extraction completion
+            feed.close();
+            yauzl.open(feed.path, { lazyEntries: true, autoClose: true }, (err, zipfile) => {
+                if (err) return reject(err);
+
+                const promises = [];
+                zipfile.readEntry();
+                zipfile.on('entry', (entry) => {
+                    const entryPath = entry.fileName;
+                    if (entryPath == 'calendar.txt') {
+                        zipfile.openReadStream(entry, (err, fileStream) => {
+                            if (err) return reject(err);
+                            promises.push(parseCalendar(fileStream));
+                        });
+                    }
+                    else if (entryPath == 'calendar_dates.txt') {
+                        zipfile.openReadStream(entry, (err, fileStream) => {
+                            if (err) return reject(err);
+                            promises.push(parseCalendarDates(fileStream));
+                        });
+                    }
+                    else if (entryPath == 'stops.txt') {
+                        zipfile.openReadStream(entry, (err, fileStream) => {
+                            if (err) return reject(err);
+                            promises.push(parseStops(fileStream));
+                        });
+                    }
+                    else if (entryPath == 'stop_times.txt') {
+                        zipfile.openReadStream(entry, (err, fileStream) => {
+                            if (err) return reject(err);
+                            promises.push(parseStopTimes(fileStream));
+                        });
+                    }
+                    else if (entryPath == 'trips.txt') {
+                        zipfile.openReadStream(entry, (err, fileStream) => {
+                            if (err) return reject(err);
+                            promises.push(parseTrips(fileStream));
+                        });
+                    }
+                    zipfile.readEntry();
+                });
+
+                zipfile.once('close', () => {
+                    Promise.all(promises).then((results) => {
+                        resolve(results.reduce(((r, c) => Object.assign(r, c)), {}));
+                    });
+                });
+            });
+        });
+        readStream.pipe(feed);
+    });
+};
+
+const download = () => {
+    const file = temp.createWriteStream('gtfs');
+    return fetch(GTFS_DATASET_URL)
+        .then((response) => { // download GTFS dataset to temp file
+            console.log(`Downloading GTFS dataset from ${GTFS_DATASET_URL}`);
+            const progressStream = createProgressStream(response.headers.get('Content-Length') * 1);
+            stream.Readable.fromWeb(response.body)
+                .pipe(progressStream) // also pipe to progress stream
+                .pipe(file); // download to temp file
+            return new Promise((resolve, reject) => {
+                file.on('error', reject);
+                file.on('finish', () => {
+                    file.close();
+                    resolve(file.path);
+                });
+            });
+        })
+        .then((filePath) => { // extract zip file
+            return new Promise((resolve, reject) => {
+                console.log(`GTFS dataset has been downloaded to ${filePath}, extracting data`);
+                yauzl.open(filePath, { lazyEntries: true, autoClose: false }, (err, zipfile) => {
+                    if (err) throw err;
+                    const promises = []; // feed extraction promises
+                    zipfile.readEntry();
+                    zipfile.on('entry', (entry) => {
+                        const entryPath = entry.fileName;
+                        // console.log(entryPath);
+                        if (!/\/$/.test(entryPath)) { // file
+                            const pathSegments = entryPath.match(/[^\/]+/g) || []; // get path segments
+                            if (
+                                pathSegments[pathSegments.length - 1] === 'google_transit.zip'
+                                && (pathSegments[0] == '1' || pathSegments[0] == '2') // 1 = regional train, 2 = metro train
+                            ) {
+                                /* GTFS feed zip file found */
+                                console.log(`Extracting GTFS feed file ${entryPath}`);
+                                zipfile.openReadStream(entry, (err, readStream) => {
+                                    if (err) throw err;
+                                    promises.push(extractFeed(readStream));
+                                });
+                            }
+                        }
+                        zipfile.readEntry(); // read next entry
+                    });
+
+                    zipfile.once('end', () => {
+                        zipfile.close();
+                        Promise.all(promises).then((results) => { // wait for all extraction promises to finish
+                            const ret = {
+                                calendar: {},
+                                calendarDates: {},
+                                stops: {},
+                                times: {},
+                                tripCalendar: {}
+                            };
+
+                            for (let i = 0; i < results.length; i++) {
+                                for (const [key, value] of Object.entries(results[i].calendar)) {
+                                    ret.calendar[`${i}_${key}`] = value;
+                                }
+                                for (const [key, value] of Object.entries(results[i].calendarDates)) {
+                                    ret.calendarDates[`${i}_${key}`] = value;
+                                }
+                                for (const [key, value] of Object.entries(results[i].tripCalendar)) {
+                                    ret.tripCalendar[key] = `${i}_${value}`;
+                                }
+                                for (const [key, value] of Object.entries(results[i].stops)) {
+                                    if (ret.stops.hasOwnProperty(key)) ret.stops[key] = ret.stops[key].concat(value);
+                                    else ret.stops[key] = value;
+                                }
+                                ret.times = { ...ret.times, ...results[i].times };
+                            }
+
+                            resolve(ret);
+                        });
+                    });
+                });
+            });
+        })
+        .catch((err) => {
+            console.error(`Error encountered downloading GTFS dataset: ${err}`);
+            throw err;
+        });
+};

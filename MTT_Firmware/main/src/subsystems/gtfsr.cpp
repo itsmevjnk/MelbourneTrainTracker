9542db2b9e91a4beec4ba99ba49ce521d437eb21
Vinh Nguyen <ngtv0404@gmail.com>
2025-10-17 19:24:23 +1100

    implement dry run to count elements for preallocation

--- MTT_Firmware/main/src/subsystems/gtfsr.cpp
+++ MTT_Firmware/main/src/subsystems/gtfsr.cpp
@@ -112,16 +112,34 @@ bool GTFSR::httpReadCallback(pb_istream_t *stream, pb_byte_t *buf, size_t count)
 // NOTE: templated functions must be defined in header file
 
 void GTFSR::updateTask(void *arg) {
+    /* dry run to count number of entries */
+    size_t initCount = 0;
+    size_t initTotalCount = 0; // including skipped events
+
+    time_t now; time(&now);
+    time_t max_time = now + CONFIG_GTFSR_WINDOW; // timestamp window
+    auto dryOnItem = [&](GTFSR::trip_update_item_t *item) { 
+        initTotalCount++;
+        if (item->arrival > max_time || (item->arrival < 0 && item->departure > max_time)) return; // skip event since it is beyond the window
+        initCount++;
+    };
+    GTFSR::getMetroTripUpdates(dryOnItem);
+    GTFSR::getVLineTripUpdates(dryOnItem);
+    ESP_LOGI(kTag, "dry run complete, got %u trip updates (%u within window)", initTotalCount, initCount);
+    Services::clearAndReserve(initCount * 2); // worst case scenario (1 arrival + 1 departure for each)
+
     while (true) {        
         Services::acquire(); Services::acquireUpdates();
-        Services::clearAndReserve(0); // TODO: is there a way to determine the number of elements, so we don't end up fragmenting our heap?
-
+        Services::clearAndReserve(0); // NOTE: services buffers are growth-only
+        
         /* for storing last departing station to match with arrival */
         uint32_t lastIDHash = 0;
         infraid_t lastDepartingStation = 0;
         time_t lastDepartingTime = -1;
 
         size_t count = 0;
+        time(&now);
+        max_time = now + CONFIG_GTFSR_WINDOW;
         auto onItem = [&](GTFSR::trip_update_item_t *item) {
             infraid_t line = (item->id[0] == '0')
                 ? INFRAID(&item->id[3]) // e.g. 02-BEG--19-T5-3637
@@ -138,9 +156,6 @@ void GTFSR::updateTask(void *arg) {
 
             ESP_LOGD(kTag, "line " INFRAID2STR_FMT " trip 0x%08x stopping at " INFRAID2STR_FMT, INFRAID2STR(line), item->idHash, INFRAID2STR(stop));
             count++;
-
-            time_t now; time(&now);
-            time_t max_time = now + CONFIG_GTFSR_WINDOW; // timestamp window
 
             if (item->arrival > max_time || (item->arrival < 0 && item->departure > max_time)) return; // skip event since it is beyond the window
             
